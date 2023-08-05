7364c29479ed5360624ba8ee36d1df548a367caa
Juan Rodas <juan.rodas.manez@gmail.com>
2023-08-05 17:55:35 -0500

    feat: Modelo inicial de base datos

---
+++ database/DB_COCKTAILS_SCRIPT.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE db_cocktails;
+
+-- MICROSERVICE 1
+
+-- TABLE: Roles
+CREATE TABLE public.roles (
+    id integer NOT NULL PRIMARY KEY,
+    name character varying(250),
+    is_deleted boolean,
+    created_at date,
+    updated_at date
+);
+
+-- TABLE: Usuarios
+CREATE TABLE public.users (
+    id integer NOT NULL PRIMARY KEY,
+    fullname character varying(250),
+    email character varying(250),
+    password character varying(250),
+    phone character varying(250),
+    role_id integer REFERENCES roles(id) ON DELETE CASCADE,
+    is_deleted boolean,
+    created_at date,
+    updated_at date
+);
+
+
+-- MICROSERVICE 2
+
+-- TABLE: Cocteles
+CREATE TABLE public.cocktails (
+    id integer NOT NULL PRIMARY KEY,
+    name character varying(250),
+    instructions character varying(250),
+    additional_notes character varying(250),
+    is_deleted boolean,
+    created_at date,
+    updated_at date
+);
+
+-- TABLE: Ingredientes
+CREATE TABLE public.ingredients (
+    id integer NOT NULL PRIMARY KEY,
+    name character varying(250),
+    is_deleted boolean,
+    created_at date,
+    updated_at date
+);
+
+-- TABLE: Ingredientes a Cocteles
+CREATE TABLE public.cocktails_ingredients (
+    cocktail_id integer REFERENCES cocktails(id) ON DELETE CASCADE,
+    ingredient_id integer REFERENCES ingredients(id) ON DELETE CASCADE
+);
