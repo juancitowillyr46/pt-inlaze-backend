0b636c438cc1f0ab251af52af048178259626733
Juan Rodas <juan.rodas.manez@gmail.com>
2023-08-06 09:57:29 -0500

    feat: Microservice user, configuración Swagger

---
+++ backend/microservice-users/src/infraestructure/config/swagger/swagger.config.ts
@@ -0,0 +1,9 @@
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export const configSwagger = new DocumentBuilder()
+// .addBearerAuth()
+.setTitle('Cocktails')
+.setDescription('Cocktails API description')
+.setVersion('1.0')
+.addTag('Cocktails')
+.build();
