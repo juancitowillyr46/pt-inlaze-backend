fedc440dae69e78ff2935b2ce4cd41e4bb0f9be8
Juan Rodas <juan.rodas.manez@gmail.com>
2023-08-19 13:33:09 -0500

    feat: Update docker compose

--- backend/api-gateway/src/infrastructure/config/swagger/swagger.config.ts
+++ backend/api-gateway/src/infrastructure/config/swagger/swagger.config.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 export const configSwagger = new DocumentBuilder()
 .addBearerAuth()
 .setTitle('Cocktails API')
-.setDescription('Cocktails API description')
+.setDescription('Cocktails API description (api gateway)')
 .setVersion('1.0')
 .addTag('Documentation')
 .build();
